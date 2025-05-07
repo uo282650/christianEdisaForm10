84280260fa5b694143908bafdffe2f8c809ce08b
Ismael Álvarez Iglesias <ismael.alvarez@edisa.com>
2025-05-07 11:28:59 +0200

    Pruebas

---
+++ src/main/java/com/edisa/formacion/mayo2025/Main.java
@@ -0,0 +1,8 @@
+package com.edisa.formacion.mayo2025;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Numero de argumentos"+String.valueOf(args.length));
+        System.out.println("Hello world! ");
+    }
+}
---
+++ src/main/java/com/edisa/formacion/mayo2025/OtroMain.java
@@ -0,0 +1,7 @@
+package com.edisa.formacion.mayo2025;
+
+public class OtroMain {
+    public static void main(String[] args) {
+        System.out.println("Hello world en otro main! ");
+    }
+}
